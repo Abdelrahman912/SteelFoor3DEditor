b41ffbff0d5651ee8a4da8a2833ab14d4ca22097
abdelrahman fathy <abdelrahmanfathe95@gmail.com>
2020-03-16 22:50:42 +0200

    Draw Steel Beam

--- Editor/js/main.js
+++ Editor/js/main.js
@@ -4,6 +4,8 @@
     let previousTime, deltaTime;
     let grid, axes;
     let orbitControls;
+    let directionalLight;
+    let lightposition = [5, 5, 5];
     //#endregion
     function init() {
         //#region 1- Creating Scene
@@ -11,7 +13,7 @@
 
         //#endregion
         //#region 2-Creating  perspective camera
-        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000); //2-Creating camera
+        camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 50000); //2-Creating camera
         camera.position.z = 20;
         camera.position.y = 10;
         camera.position.x = 0;
@@ -26,19 +28,35 @@
         document.body.appendChild(renderer.domElement); //append canvas tag to html
 
         //#endregion
+        //#region Light
+        directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+        directionalLight.position.set(lightposition[0], lightposition[1], lightposition[2]);
+        scene.add(directionalLight);
+        //#endregion
+
 
         //#region Creating Grids
-        grid = new THREE.GridHelper(20, 20, 0x000000, 0xffffff);
+        grid = new THREE.GridHelper(50, 20, 0x000000, 0xffffff);
         scene.add(grid);
-        axes = new THREE.AxesHelper(10);
+        axes = new THREE.AxesHelper(500);
         scene.add(axes);
 
-        let myGrid = new Grid(scene, 20, 10, 4, 5);
+        let myGrid = new Grid(scene, 50, 60, 4, 5);
         //#endregion
         //#region Orbit controls
         orbitControls = new THREE.OrbitControls(camera, renderer.domElement); //renderer.domElement is the canvas
 
         //#endregion
+
+        //#region Drawing Beams
+        //let solid = new Solid(scene, null, null, new THREE.Vector3(0, -Math.PI / 2, 0), null);
+        let beam = new ISection(scene, null, null, 60);
+        let beam2 = new ISection(scene, null, new THREE.Vector3(50, 0, 0), 60);
+        let beam3 = new ISection(scene, null, new THREE.Vector3(50, 0, 60), 60)
+
+        //#endregion
+
+
     }
 
     function update() {
@@ -59,42 +77,3 @@
     init();
     loop(0);
 })();
-
-
-function Grid(scene, spaceX, spaceZ, numberInX, numberInZ) {
-    let shift = 5 / 2;
-    this.lineLengthInX = spaceX * (numberInX - 1) + 5;
-    this.lineLengthInZ = spaceZ * (numberInZ - 1) + 5;
-    this.linesInX = [];
-    this.linesInZ = [];
-    let accumlateSpaceX = 0;
-    let accumlateSpaceZ = 0;
-
-    for (let i = 0; i < numberInX; i++) {
-        this.linesInX[i] = new Line(scene, new THREE.Vector3(accumlateSpaceX, 0, -shift), new THREE.Vector3(0, 0, 1), this.lineLengthInZ);
-        accumlateSpaceX += spaceX;
-    }
-    for (let i = 0; i < numberInZ; i++) {
-
-        this.linesInZ[i] = new Line(scene, new THREE.Vector3(-shift, 0, accumlateSpaceZ), new THREE.Vector3(1, 0, 0), this.lineLengthInX);
-        accumlateSpaceZ += spaceZ;
-    }
-
-
-}
-
-function Line(scene, startPoint, direction, length) {
-    this.startPoint = startPoint || new THREE.Vector3(0, 0, 0);
-    this.direction = direction || new THREE.Vector3(1, 0, 0);
-    this.length = length || 1000;
-    this.endPoint = new THREE.Vector3(this.startPoint.x + this.length * this.direction.x, this.startPoint.y + this.length * this.direction.y, this.startPoint.z + this.length * this.direction.z);
-    this.material = new THREE.LineDashedMaterial({
-        color: 0xff33ff,
-        gapSize: 10,
-        dashSize: 3,
-        scale: 1
-    });
-    this.geometry = new THREE.BufferGeometry().setFromPoints([this.startPoint, this.endPoint]);
-    this.line = new THREE.Line(this.geometry, this.material);
-    scene.add(this.line);
-}
